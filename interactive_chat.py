cf326d0fb0cab410e4193c1baa6df57a37065cd0
KeiranCantilina <keirancantilina@gmail.com>
2026-04-15 16:09:27 -0400

    Added markdown formatting

--- interactive_chat.py
+++ interactive_chat.py
@@ -5,18 +5,24 @@ import readline
 
 from llama_cpp import Llama
 from termcolor import cprint
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.live import Live
 
+console = Console()
 
 #model_path = "/mnt/beegfs/cantilk/.cache/huggingface/hub/models--lmstudio-community--Qwen3-30B-A3B-GGUF/snapshots/98773a4c5f7f32199dd3a2a484deff0864a8d6f2/Qwen3-30B-A3B-Q4_K_M.gguf"
 
-model_path = "/mnt/beegfs/cantilk/.cache/huggingface/hub/models--lmstudio-community--Qwen3.5-122B-A10B-GGUF/snapshots/a5ba02062d2fd26c1f17000571ffc86e7abf4c92/Qwen3.5-122B-A10B-Q4_K_M-00001-of-00002.gguf"
+#model_path = "/mnt/beegfs/cantilk/.cache/huggingface/hub/models--lmstudio-community--Qwen3.5-122B-A10B-GGUF/snapshots/a5ba02062d2fd26c1f17000571ffc86e7abf4c92/Qwen3.5-122B-A10B-Q4_K_M-00001-of-00002.gguf"
+
+model_path = "/mnt/beegfs/cantilk/gpt-oss-120b-MXFP4-00001-of-00002.gguf"
 
 print_bot = lambda x: cprint(x, 'green', end='')
 
 def main(model_path, verbose=False, n_threads=16, n_gpu_layers=-1):
     params = {
         "model_path": model_path,
-        "n_ctx": 32768, # Adjust based on your VRAM/RAM
+        "n_ctx": 131072, # Adjust based on your VRAM/RAM
         "n_threads": int(n_threads),
         "verbose": verbose,
         "n_gpu_layers": int(n_gpu_layers),
@@ -62,12 +68,15 @@ def main(model_path, verbose=False, n_threads=16, n_gpu_layers=-1):
         )
 
         full_response = ""
-        for chunk in stream:
-            if "content" in chunk["choices"][0]["delta"]:
-                token = chunk["choices"][0]["delta"]["content"]
-                full_response += token
-                print_bot(token)
-                sys.stdout.flush()
+        with Live(console=console, refresh_per_second=10) as live:
+                for chunk in stream:
+                        if "content" in chunk["choices"][0]["delta"]:
+                                token = chunk["choices"][0]["delta"]["content"]
+                                full_response += token
+                                # print_bot(token)
+                                # sys.stdout.flush()
+                                live.update(Markdown(full_response))
+
 
         # Add assistant response to history
         history.append({"role": "assistant", "content": full_response})
